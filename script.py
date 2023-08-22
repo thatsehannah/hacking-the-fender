a641687f828cd05bda21d73a9c118a8690a6a940
E. Hannah <echannah631@gmail.com>
2023-08-22 18:53:33 -0500

    adding hacker signature file

--- script.py
+++ script.py
@@ -19,3 +19,26 @@ with open("compromised_users.txt", 'w') as compromised_user_file:
 with open("boss_message.json", 'w') as boss_message:
   boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
   json.dump(boss_message_dict, boss_message)
+
+# SCRAMBLING THE PASSWORD
+with open("new_passwords.csv", 'w') as new_passwords_obj:
+  slash_null_sig = """ # says "U Got Hacked - Slash Null"
+    _  _     ___   __  ____             
+    / )( \   / __) /  \(_  _)            
+    ) \/ (  ( (_ \(  O ) )(              
+    \____/   \___/ \__/ (__)             
+    _  _   __    ___  __ _  ____  ____  
+    / )( \ / _\  / __)(  / )(  __)(    \ 
+    ) __ (/    \( (__  )  (  ) _)  ) D ( 
+    \_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+            ____  __     __   ____  _  _ 
+    ___   / ___)(  )   / _\ / ___)/ )( \
+    (___)  \___ \/ (_/\/    \\___ \) __ (
+          (____/\____/\_/\_/(____/\_)(_/
+    __ _  _  _  __    __                
+    (  ( \/ )( \(  )  (  )               
+    /    /) \/ (/ (_/\/ (_/\             
+    \_)__)\____/\____/\____/
+  """
+  
+  new_passwords_obj.write(slash_null_sig)
